20b8c1eb8b9bfb6b61b888087e12a08971c6a437
Amrhanysayed <149177595+Amrhanysayed@users.noreply.github.com>
2024-04-05 19:27:08 +0200

    war started sound

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
 #include"Game.h"
 #include"../7rb-imbaba/Units/EarthSoldier.h"
+#include<string>
+#include<windows.h>
+#include<mmsystem.h>
+#pragma comment (lib,"winmm.lib")
 using namespace std;
 int main()
 {
-  
-   // EarthSoldier s1;
+  // sound over 
+    string v = "War-strated.wav ";
+    PlaySoundA(v.c_str(), NULL, SND_FILENAME);
+   //////////////////////////////////////////
+    // EarthSoldier s1;
    //// Unit* p;
    // s1.PrintInfo();
     /////////////////////SCRIPT TESTING RANDGEN///////////////////////////
+    
+  
+  
     Game* gPtr = new Game('a');
+   
     for(int i = 0;i<50;i++)
         gPtr->NextTimeStep();
     delete gPtr;
